60dd1fd268855a56607a147715fbd4a6e96af9e6
mfunyu <mfunyu@student.42tokyo.jp>
2022-01-03 19:57:50 +0900

    style: formatter

--- vector_main.cpp
+++ vector_main.cpp
@@ -1,20 +1,21 @@
-#include <vector>
-#include <string>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
 namespace ft = std;
 
 /*
 ** Iterators
 ** begin	: Return iterator to beginning (public member function)
 ** end		: Return iterator to end (public member function)
-** rbegin	: Return reverse iterator to reverse beginning (public member function)
+** rbegin	: Return reverse iterator to reverse beginning (public member
+** function)
 ** rend		: Return reverse iterator to reverse end (public member function)
 */
 template <typename IteratorType>
-void	_test_iterators(std::string iterator_type, IteratorType iterator)
+void _test_iterators(std::string iterator_type, IteratorType iterator)
 {
-	IteratorType copy = iterator;
+	IteratorType copy  = iterator;
 	IteratorType copy2 = iterator;
 	IteratorType copy3 = iterator;
 
@@ -28,7 +29,7 @@ void	_test_iterators(std::string iterator_type, IteratorType iterator)
 }
 
 template <typename T>
-void	test_iterators(ft::vector<T> test)
+void test_iterators(ft::vector<T> test)
 {
 	_test_iterators("begin", test.begin());
 	_test_iterators("end", test.end());
@@ -36,11 +37,11 @@ void	test_iterators(ft::vector<T> test)
 	_test_iterators("rend", test.rend());
 }
 
-void	test()
+void test()
 {
 	ft::vector<int> vector_int(7, 50);
 
-	ft::vector<int>::iterator it = vector_int.begin();
+	ft::vector<int>::iterator it   = vector_int.begin();
 	ft::vector<int>::iterator it_e = vector_int.end();
 	std::cout << "<vector>" << std::endl;
 	for (; it != it_e; ++it) {
@@ -51,7 +52,7 @@ void	test()
 	test_iterators<int>(vector_int);
 }
 
-int		main(int ac, char **av)
+int main(int ac, char **av)
 {
 	try {
 		std::string arg = (ac > 1 ? av[1] : "");
