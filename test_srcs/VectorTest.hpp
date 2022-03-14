5da10428758412322ff45861f219e74eebce6c03
mfunyu <mfunyu@student.42tokyo.jp>
2022-03-14 14:03:30 +0900

    add: all test func names

--- test_srcs/VectorTest.hpp
+++ test_srcs/VectorTest.hpp
@@ -18,10 +18,39 @@ void vector_get_allocator();
 void vector_at();
 void vector_subscript_operator();
 void vector_front();
+void vector_back();
+void vector_data();
 
+/* Iterators */
 void vector_begin();
-void vector_size();
+void vector_end();
+void vector_rbegin();
+void vector_rend();
+
+/* Capacity */
 void vector_empty();
+void vector_size();
+void vector_maxsize();
+void vector_reverse();
+void vector_capacity();
+
+/* Modifiers */
+void vector_clear();
+void vector_insert();
+void vector_erase();
+void vector_push_back();
+void vector_pop_back();
+void vector_resize();
+void vector_swap();
+
+/* Non-member functions */
+void vector_operator_e();
+void vector_operator_ne();
+void vector_operator_l();
+void vector_operator_le();
+void vector_operator_g();
+void vector_operator_ge();
+void vector_std_swap();
 
 extern t_unit_tests func_test_table[TABLE_SIZE];
 
