519ab76d46aada5216634d71449db796ad00cb5d
mfunyu <mfunyu@student.42tokyo.jp>
2022-03-15 20:51:05 +0900

    add: vector.hpp

---
+++ includes/vector.hpp
@@ -0,0 +1,19 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <iostream>
+#include <memory>
+
+namespace ft {
+
+template <class T, class Allocator = std::allocator<T> >
+class vector
+{
+  private:
+  public:
+};
+
+
+} // namespace ft
+
+#endif /* VECTOR_HPP */
