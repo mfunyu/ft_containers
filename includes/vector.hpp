bf9daec1b598287da70392a93fb4d1dc33da2a46
mfunyu <mfunyu@student.42tokyo.jp>
2022-03-15 20:58:31 +0900

    add: class vector typedefs

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -11,6 +11,21 @@ class vector
 {
   private:
   public:
+	typedef T                        value_type;
+	typedef Allocator                allocator_type;
+	typedef std::size_t              size_type;
+	typedef std::ptrdiff_t           difference_type;
+	typedef value_type&              reference;
+	typedef const value_type&        const_reference;
+	typedef Allocator::pointer       pointer;
+	typedef Allocator::const_pointer const_pointer;
+
+	/*
+	typedef iterator;
+	typedef const_iterator;
+	typedef ft::reverse_iterator<iterator> reverse_iterator;
+	typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+	*/
 };
 
 
