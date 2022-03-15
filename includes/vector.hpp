1e289d63e446d64b4556f8409dedc239d6e1c1b1
mfunyu <mfunyu@student.42tokyo.jp>
2022-03-15 21:18:42 +0900

    update: add typename for typedef

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -11,14 +11,14 @@ class vector
 {
   private:
   public:
-	typedef T                        value_type;
-	typedef Allocator                allocator_type;
-	typedef std::size_t              size_type;
-	typedef std::ptrdiff_t           difference_type;
-	typedef value_type&              reference;
-	typedef const value_type&        const_reference;
-	typedef Allocator::pointer       pointer;
-	typedef Allocator::const_pointer const_pointer;
+	typedef T                                 value_type;
+	typedef Allocator                         allocator_type;
+	typedef std::size_t                       size_type;
+	typedef std::ptrdiff_t                    difference_type;
+	typedef value_type&                       reference;
+	typedef const value_type&                 const_reference;
+	typedef typename Allocator::pointer       pointer;
+	typedef typename Allocator::const_pointer const_pointer;
 
 	/*
 	typedef iterator;
