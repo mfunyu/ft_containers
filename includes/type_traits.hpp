0fdfc77f80358c7ffd7772cb5e10a2cc990ea52f
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-05 22:01:38 +0900

    add: enable_if

---
+++ includes/type_traits.hpp
@@ -0,0 +1,13 @@
+#ifndef TYPE_TRAITS_HPP
+#define TYPE_TRAITS_HPP
+
+namespace ft {
+
+template <bool B, class T = void>
+struct enable_if
+{
+};
+
+} // namespace ft
+
+#endif /* TYPE_TRAITS_HPP */
