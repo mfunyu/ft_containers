7ff0b985da06be48b1bea6c84949aa2f83de75db
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-08 19:32:30 +0900

    update: add and set capacity tests undefined

--- test_srcs/vector/VectorTest_Capacity.cpp
+++ test_srcs/vector/VectorTest_Capacity.cpp
@@ -175,7 +175,7 @@ void _vector_capacity_ratio()
 	set_explanation_("capacity not 0 at size 0");
 	ft::vector<int> ft;
 	size_t          cap = ft.capacity();
-	UnitTester::assert_(cap == 0);
+	UnitTester::assert_diff_(cap == 0);
 
 	size_t size = 4242;
 	for (size_t i = 0; i < size; ++i) {
@@ -183,31 +183,54 @@ void _vector_capacity_ratio()
 			if (i == 1) {
 				cap = ft.capacity();
 				set_explanation_("capacity is not 1 at size 1");
-				UnitTester::assert_(cap == 1);
+				UnitTester::assert_diff_(cap == 1);
 				continue;
 			}
 			set_explanation_("capacity is not doubled the size");
-			UnitTester::assert_(ft.capacity() / static_cast<float>(cap) == 2);
+			UnitTester::assert_diff_(ft.capacity() / static_cast<float>(cap) == 2);
 			cap = ft.capacity();
 		}
 		ft.push_back(i);
 	}
 }
 
-void _vector_capacity_compare()
+void _vector_capacity_compare_empty()
 {
-	set_explanation_("result differs from std");
+	set_explanation_("capacity differs from std (undefined behavior)");
 	ft::vector<int>  ft;
 	std::vector<int> std;
-	_set_compare_vectors(ft, std);
 
-	UnitTester::assert_(ft.capacity() == std.capacity());
+	UnitTester::assert_diff_(ft.capacity() == std.capacity());
+}
+
+void _vector_capacity_compare_even()
+{
+	set_explanation_("capacity differs from std (undefined behavior)");
+	ft::vector<int>  ft;
+	std::vector<int> std;
+	size_t           size = 12;
+	_set_compare_vectors(ft, std, size);
+
+	UnitTester::assert_diff_(ft.capacity() == std.capacity());
+}
+
+void _vector_capacity_compare_odd()
+{
+	set_explanation_("capacity differs from std (undefined behavior)");
+	ft::vector<int>  ft;
+	std::vector<int> std;
+	size_t           size = 13;
+	_set_compare_vectors(ft, std, size);
+
+	UnitTester::assert_diff_(ft.capacity() == std.capacity());
 }
 
 void vector_capacity()
 {
 	load_subtest_(_vector_capacity_ratio);
-	load_subtest_(_vector_capacity_compare);
+	load_subtest_(_vector_capacity_compare_empty);
+	load_subtest_(_vector_capacity_compare_odd);
+	load_subtest_(_vector_capacity_compare_even);
 }
 
 } // namespace VectorTest
