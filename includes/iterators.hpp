584432a1b25130658367783863558bcf907cadf9
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-11 16:11:52 +0900

    update

--- includes/iterators.hpp
+++ includes/iterators.hpp
@@ -10,12 +10,12 @@ template <class T>
 class random_access_iterator : public std::iterator<std::random_access_iterator_tag, T>
 {
   public:
-	typedef T*                                              iterator_type;
-	typedef typename iterator_traits<T*>::difference_type   difference_type;
-	typedef typename iterator_traits<T*>::value_type        value_type;
-	typedef typename iterator_traits<T*>::pointer           pointer;
-	typedef typename iterator_traits<T*>::reference         reference;
-	typedef typename iterator_traits<T*>::iterator_category iterator_category;
+	typedef T*                                                         iterator_type;
+	typedef typename iterator_traits<iterator_type>::difference_type   difference_type;
+	typedef typename iterator_traits<iterator_type>::value_type        value_type;
+	typedef typename iterator_traits<iterator_type>::pointer           pointer;
+	typedef typename iterator_traits<iterator_type>::reference         reference;
+	typedef typename iterator_traits<iterator_type>::iterator_category iterator_category;
 
   private:
 	pointer __i;
