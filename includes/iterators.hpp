9fbf771d2ff78849a9f53c71b2e2213b5665b1d0
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-11 16:11:15 +0900

    add: iterator_type & base()

--- includes/iterators.hpp
+++ includes/iterators.hpp
@@ -10,6 +10,7 @@ template <class T>
 class random_access_iterator : public std::iterator<std::random_access_iterator_tag, T>
 {
   public:
+	typedef T*                                              iterator_type;
 	typedef typename iterator_traits<T*>::difference_type   difference_type;
 	typedef typename iterator_traits<T*>::value_type        value_type;
 	typedef typename iterator_traits<T*>::pointer           pointer;
@@ -32,10 +33,10 @@ class random_access_iterator : public std::iterator<std::random_access_iterator_
 		__i = other.__i;
 		return *this;
 	}
-
-	reference operator*() const { return *__i; }
-	pointer   operator->() const { return __i; }
-	reference operator[](difference_type n) const { return __i[n]; }
+	iterator_type base() const { return __i; }
+	reference     operator*() const { return *__i; }
+	pointer       operator->() const { return __i; }
+	reference     operator[](difference_type n) const { return __i[n]; }
 
 	/* ------------------------- Advances / Decrements ------------------------- */
 	random_access_iterator& operator++()
