ac94ff1e671c23fdcbd6f41559065d016731e3e2
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-12 15:59:40 +0900

    add: constructor test

--- test_srcs/map/MapTest_Basic.cpp
+++ test_srcs/map/MapTest_Basic.cpp
@@ -1,5 +1,8 @@
+#include "MapTest.hpp"
 #include "UnitTester.hpp"
+#include <iostream>
 #include <map>
+#include <vector>
 
 namespace MapTest {
 
@@ -7,11 +10,42 @@ namespace MapTest {
 /*                                 constructor                                */
 /* -------------------------------------------------------------------------- */
 
-void _map_constructor_default() {}
+void _map_constructor_default()
+{
+	set_explanation_("result differs from std behavior");
+	ft::map<int, int>  ft;
+	std::map<int, int> std;
+	_compare_maps(ft, std);
+}
+
+void _map_constructor_range()
+{
+	set_explanation_("result differs from std behavior");
+	size_t             size     = 14;
+	std::map<int, int> map_base = _set_map(size);
+
+	ft::map<int, int>  ft(map_base.begin(), map_base.end());
+	std::map<int, int> std(map_base.begin(), map_base.end());
+	_compare_maps(ft, std);
+}
+
+void _map_constructor_copy()
+{
+	set_explanation_("copy result differs from std behavior");
+	ft::map<int, std::string>  ft;
+	std::map<int, std::string> std;
+	_set_compare_maps(ft, std);
+
+	ft::map<int, std::string>  ft_cpy(ft);
+	std::map<int, std::string> std_cpy(std);
+	_compare_maps(ft_cpy, std_cpy);
+}
 
 void map_constructor()
 {
 	load_subtest_(_map_constructor_default);
+	load_subtest_(_map_constructor_range);
+	load_subtest_(_map_constructor_copy);
 }
 
 } // namespace MapTest
