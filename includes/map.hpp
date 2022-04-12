105f1e0ac23f9e94c3563a3073b02fecec41cab0
mfunyu <mfunyu@student.42tokyo.jp>
2022-04-12 15:51:55 +0900

    add: style map class for compilation

--- includes/map.hpp
+++ includes/map.hpp
@@ -1,6 +1,10 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+namespace ft {
+
+template <class Key, class T, class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> > >
 class map
 {
   private:
@@ -10,5 +14,7 @@ class map
 	map(map const& other);
 	map& operator=(map const& other);
 };
+
+} // namespace ft
 
 #endif /* MAP_HPP */
